0a609ee0cd4a2f8f14efbfe1ca8970205be6b880
henryapplegate <ha7applegate@gmail.com>
2025-10-05 13:27:48 -0400

    Update TempConverter.java

--- src/main/java/TempConverter.java
+++ src/main/java/TempConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TempConverter {
     public static void main(String[] args) {
         // TODO: Implement temperature converter
@@ -7,5 +9,52 @@ public class TempConverter {
         // - Use formulas: C = (F - 32) * 5/9 and F = C * 9/5 + 32
         // - Display conversion results
         // - Handle invalid menu choices
+
+        Scanner sc = new Scanner(System.in);
+        boolean menu = true;
+        double temp = 0.0;
+
+        while (menu)
+        {
+            System.out.println("Choose conversion direction:");
+            System.out.println("1. Convert Fahrenheit to Celsius");
+            System.out.println("2. Convert Celsius to Fahrenheit");
+            System.out.println("3. Exit");
+            System.out.print("\nEnter your choice (1, 2, or 3):  ");
+            int choice = sc.nextInt();
+
+            switch (choice)
+            {
+                default:
+                {
+                    System.out.println("Error");
+                    break;
+                }
+                case 1:
+                    System.out.print("\nEnter temperature in Fahrenheit: ");
+                    temp = sc.nextDouble();
+                    System.out.println("\nConversion Results:");
+                    System.out.println("Fahrenheit: " + temp);
+                    temp = (temp-32) * (5/9);
+                    System.out.println("Celsius: " + temp);
+                    break;
+                case 2:
+                {
+                    System.out.print("\nEnter temperature in Celsius: ");
+                    temp = sc.nextDouble();
+                    System.out.println("\nConversion Results:");
+                    System.out.println("Celsius: " + temp);
+                    temp = temp * (9/5) + 32;
+                    System.out.println("Fahrenheit: " + temp);
+                    break;
+                }
+                case 3:
+                {
+                    System.out.println("Goodbye!");
+                    menu = false;
+                    break;
+                }
+            }
+        }
     }
 }
